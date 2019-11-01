6ddc404da852e42e71eed5834e57426eb1a12944
osullid3 <david.osullivan@vuw.ac.nz>
2019-11-01 15:05:18 +1300

    changed to svg graphic

--- output/invasion-by-lambda-pldd.R
+++ output/invasion-by-lambda-pldd.R
@@ -1,10 +1,13 @@
+library(dplyr)
+library(ggplot2)
+
 setwd('~/Documents/code/working/wasps/output')
 wasps <- read.csv("wasps experiment-base-1-table.csv", skip=6)
 
 wasps <- select(wasps, 1:5,21:23)
-
+svg("invasion-by-lambda-pldd.svg")
 ggplot(wasps) + 
   geom_bin2d(aes(x=X.step., y=prop.occupied), bins=10) + 
   scale_fill_distiller(palette='Reds', direction=1) + 
   facet_grid(p.ldd ~ lambda.1, labeller=label_both, as.table=FALSE)
-ggsave("invasion-by-lambda-pldd.png")
+dev.off()
