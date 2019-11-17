e3c4acabafd20f6c5ebb5c38c996f61e807ca6d2
DOSull <david.osullivan@vuw.ac.nz>
2019-11-18 10:06:28 +1300

    couple more plots addded

--- output/invasion-by-lambda-pldd.R
+++ output/invasion-by-lambda-pldd.R
@@ -53,11 +53,18 @@ wasps.t <- wasps %>%
   filter(total.pop > 0) %>%
   mutate(LDD = as.factor(p.ldd))
 
+svg("time-series-by-lambda-pldd.svg")
+ggplot(wasps, aes(x=X.step., y=prop.occupied)) + 
+  geom_line(aes(group=X.run.number.), color='grey', lwd=.1) + 
+  xlim(0, 50) + ylim(0, 1) +
+  scale_fill_distiller(palette='Reds', direction=1) + 
+  geom_smooth() +
+  facet_grid(p.ldd ~ lambda.1, labeller=label_both, as.table=FALSE)
+dev.off()
 
 svg("time-to-95-by-lambda-pldd.svg")
 ggplot(wasps.t) + 
   geom_smooth(aes(x=lambda.1, y=X.step., group=LDD, color=LDD))
-
 dev.off()
 
 
