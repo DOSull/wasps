9a714beb97ed2d325694d6d0c687c732e9967a1a
osullid3 <david.osullivan@vuw.ac.nz>
2019-11-19 13:32:36 +1300

    updated plots to include r-mean

--- output/invasion-by-lambda-pldd.R
+++ output/invasion-by-lambda-pldd.R
@@ -44,27 +44,29 @@ library(ggplot2)
 setwd('~/Documents/code/working/wasps/output/')
 wasps <- read.csv("wasps experiment-nozero-table.csv", skip=6)
 
-wasps <- select(wasps, 1:5,11, 21:23)
+wasps <- select(wasps, 1:5,11, 15, 21:23)
 
 wasps.t <- wasps %>% 
-  group_by(X.run.number.) %>%
+  group_by(X.run.number., r.mean) %>%
   summarise_at('X.step.', max) %>%
   merge(wasps) %>%
   filter(total.pop > 0) %>%
-  mutate(LDD = as.factor(p.ldd))
+  mutate(P.LDD = as.factor(p.ldd),
+         R.MEAN = as.factor(r.mean))
 
 svg("time-series-by-lambda-pldd.svg")
-ggplot(wasps, aes(x=X.step., y=prop.occupied)) + 
-  geom_line(aes(group=X.run.number.), color='grey', lwd=.1) + 
+ggplot(wasps, aes(x=X.step., y=prop.occupied, color=r.mean)) + 
+  geom_line(aes(group=X.run.number.), lwd=.2, alpha=0.35) + 
   xlim(0, 50) + ylim(0, 1) +
-  scale_fill_distiller(palette='Reds', direction=1) + 
-  geom_smooth() +
+  scale_color_viridis_c(option='D') + 
   facet_grid(p.ldd ~ lambda.1, labeller=label_both, as.table=FALSE)
 dev.off()
 
 svg("time-to-95-by-lambda-pldd.svg")
 ggplot(wasps.t) + 
-  geom_smooth(aes(x=lambda.1, y=X.step., group=LDD, color=LDD))
+  geom_smooth(aes(x=lambda.1, y=X.step., group=p.ldd, color=P.LDD)) +
+  scale_color_viridis_d(option='C', direction=-1) +
+  facet_wrap(vars(r.mean), nrow=1)
 dev.off()
 
 
